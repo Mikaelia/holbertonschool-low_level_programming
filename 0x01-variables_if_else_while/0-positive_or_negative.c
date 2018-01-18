04078d33b39ca8a09f1a9255ef81d57a8a962234
Mikaela Gurney <354@holbertonschool.com>
2018-01-18 20:54:29 +0000

    Add project folder and first task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n)
+	return (0);
+}
