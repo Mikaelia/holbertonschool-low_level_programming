af9c8f3c6456602b5cb08f58b1d9dbbd50e47c61
Mikaelia <mikaelagurney@gmail.com>
2018-01-23 09:10:32 +0000

    Add task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 	{
 		int i;
 
-		for (i = 97; i <= 122; i++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
