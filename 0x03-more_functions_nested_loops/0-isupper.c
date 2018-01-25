21465d6bd3de9122edef35ce9224c5592adbef60
Mikaelia <mikaelagurney@gmail.com>
2018-01-25 21:49:06 +0000

    Fix header

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ *_isupper - check thde code for Holberton School students.
+ * @c: letter ot be tested
  * Return: Always 0
  *
  */
 int _isupper(int c)
 {
-    if (c > 'A' && c > 'Z')
-        return (1);
-    else
-        return (0);
+	if (c > 'A' && c > 'Z')
+		return (1);
+	else
+		return (0);
 }
