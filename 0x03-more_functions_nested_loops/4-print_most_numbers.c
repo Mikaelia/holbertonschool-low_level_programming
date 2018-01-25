eb4d5717565aeeb168fb92e47cf252259b5155dd
Mikaelia <mikaelagurney@gmail.com>
2018-01-25 23:47:35 +0000

    Add task 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * print_numbers - Print numbers 0 - 9
+  *
+  * Return: void
+  */
+void print_most_numbers(void) 
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		if (i != '2' && i != '4')
+			_putchar(i);
+	_putchar('\n');
+}
