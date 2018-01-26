d3b717ff20075d9f2491a0e7510c2168c405e77f
Mikaelia <mikaelagurney@gmail.com>
2018-01-26 04:35:55 +0000

    Add task 8

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - multiply two integers
+ * @size: Size of the square
+ *
+ * Return: Always void
+ *
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+		_putchar('\n');
+
+		}
+	}
+	else
+		_putchar('\n');
+}
