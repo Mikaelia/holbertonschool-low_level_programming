8724392309768dd4c5d9bb6c1cb910e6101a4f40
Mikaelia <mikaelagurney@gmail.com>
2018-01-26 03:12:22 +0000

    Fix task 7

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,23 @@ void print_diagonal(int n)
 	int i, s;
 
 	if (n > 0)
+	{
+
 		for (i = 0; i < n; i++)
 		{
-			for (s = 0; s <= i; s++)
+			for (s = 1; s <= i; s++)
+			{
+				if (s != i)
 				_putchar(' ');
-			_putchar('\\');
-			if (s != n)
-			_putchar('\n');
+				else if (s == i)
+				{
+
+					_putchar('\\');
+					_putchar('\n');
+				}
+			}
 		}
-	_putchar('\n');
+	}
+	else
+		_putchar('\n');
 }
