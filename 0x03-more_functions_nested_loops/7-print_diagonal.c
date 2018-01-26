9796b6e419391a677f2421da3ec64aa6c7c2c00a
Mikaelia <mikaelagurney@gmail.com>
2018-01-26 01:26:20 +0000

    Add task 7

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+  * print_diagonal-  draws straight line in terminal
+  * @n: number of times the slash is printed
+  * Return: void
+  */
+void print_diagonal(int n)
+{
+	int i, s;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+		{
+			for (s = 0; s <= i; s++)
+				_putchar(' ');
+			_putchar(92);
+			if (s != n)
+			_putchar('\n');
+		}
+	_putchar('\n');
+}
