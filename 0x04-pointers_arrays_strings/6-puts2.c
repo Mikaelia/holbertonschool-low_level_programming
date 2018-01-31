526f425d21d512ef57850c5cb44ca6de7db20900
Mikaelia <mikaelagurney@gmail.com>
2018-01-31 00:18:37 +0000

    Add task 6

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * puts2 - prints one char out of 2 of a string
+ *
+ * @str: string object
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int strlen;
+	int j;
+
+	i = 0;
+	strlen = 0;
+
+	while (str[i] != '\0')
+	{
+		strlen++;
+		i++;
+	}
+	j = 0;
+
+	while (j <= (strlen - 2))
+	{
+		_putchar((str[j]));
+		j += 2;
+	}
+	_putchar('\n');
+}
