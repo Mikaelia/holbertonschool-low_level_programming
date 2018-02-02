51aadd165b51f1ffde023513c19516c42031f50b
Mikaelia <mikaelagurney@gmail.com>
2018-02-02 04:49:58 +0000

    Add task 6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -7,5 +7,22 @@
  */
 char *cap_string(char *s)
 {
-	if s[i - 1] != 
+	int i;
+	int j;
+	char sym[13] = {' ', '\t', '\n', ',', ';',
+'.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] -= 32;
+		for (j = 0; j < 13; j++)
+			while (s[i] == sym[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+				break;
+			}
+	}
+	return (s);
 }
