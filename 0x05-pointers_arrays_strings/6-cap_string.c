f6daddd501cb9fdb88e8ef93ac02dec013b3d5f5
Mikaelia <mikaelagurney@gmail.com>
2018-02-02 03:01:46 +0000

    Add task 7

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: string
+ *
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	if s[i - 1] != 
+}
---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *leet - encodes a string into 1337
+ *
+ * @s: string
+ *
+ *
+ * Return: char
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+
+	char lower[6] = "aeotl";
+	char upper[6] = "AEOTL";
+	int nums[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for(j = 0; j < 5; j++)
+		{
+			if (s[i] == lower[j] || s[i] == upper[j])
+				s[i] = nums[j];
+		}
+	}
+	return (s);
+}
