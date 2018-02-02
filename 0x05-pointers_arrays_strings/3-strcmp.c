919852f1bd435856f21eebd17e02a09a8edbe57e
Mikaelia <mikaelagurney@gmail.com>
2018-02-02 00:11:05 +0000

    Fix format task 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -11,18 +11,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	while (*s1 != '\0' && *s1 == *s2)
 	{
-		if (*s1 < *s2)
-			return (-15);
-		if (*s1 > *s2)
-			return (15);
-		if (*s1 == *s2)
-			return (0);
 		s1++;
 		s2++;
 	}
 	if (*s2 != '\0')
-		return (-15);
+		return (*s1 - *s2);
 	return (0);
 }
