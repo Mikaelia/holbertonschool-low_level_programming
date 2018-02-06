075b32bbe9a8b7415e27c10f194ceb35916a7bab
Mikaelia <mikaelagurney@gmail.com>
2018-02-06 01:48:03 +0000

    Add task 4

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
-   * main - check the code for Holberton School students.
-    *
-     * Return: Always 0.
-      */
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    char *s = "hello, world";
-	        char *f = "oleh";
-		    unsigned int n;
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-		        n = _strspn(s, f);
-			    printf("%u\n", n);
-			        return (0);
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    n = strspn(s, f);
+    printf("%u\n", n);
 
+    printf( "%u\n", strspn(s, '\0')  );
+    return (0);
 }
