acc439264975333eb6130ae543264373f301fc14
Mikaelia <mikaelagurney@gmail.com>
2018-02-07 20:25:25 +0000

    Add task 1

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  * _puts_recursion - prints a string
+  * @s: string to print
+  *
+  *
+  * Return: void
+  */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
+
+}
---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  *
+  *
+  *
+  *
+  * Return:
+  */
+void _puts_recursion(char *s)
+{
+
+}
