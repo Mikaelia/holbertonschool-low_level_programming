d1f9b5ac53e7e3a3471595cc6b6564d65d16f43a
Mikaelia <mikaelagurney@gmail.com>
2018-02-07 22:57:49 +0000

    Add task 3

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
 /**
+  * factorial - returns factorial of number
+  * @n: number to take factorial of
   *
   *
-  *
-  *
-  * Return:
+  * Return: factorial
   */
-void _puts_recursion(char *s)
+int factorial(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial((n - 1)));
 }
