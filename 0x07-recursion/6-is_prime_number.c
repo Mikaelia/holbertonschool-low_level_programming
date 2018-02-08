719a82d40a4bff304ade55a6618d0679b9534ebe
Mikaelia <mikaelagurney@gmail.com>
2018-02-08 01:04:10 +0000

    Add task 6

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,12 +1,38 @@
+/**
+  * checkprime - returns prime number
+  * @factor: factor test prime divisibility
+  * @n: number to test for prime
+  *
+  * Return: 1 for prime, 0 otherwise
+  */
+int checkprime(int factor, int n)
+{
+	if (factor < n)
+	{
+		if (n % factor == 0)
+			return (0);
+		return (checkprime(++factor, n));
+	}
+		return (1);
+}
+
+
 #include "holberton.h"
 /**
+  * is_prime_number - calls checkprime
+  * @n: number to test for prime
   *
   *
-  *
-  *
-  * Return:
+  * Return: 1 for prime, 0 otherwise
   */
-void _puts_recursion(char *s)
+int is_prime_number(int n)
 {
+	int factor;
+
+	if (n <= 1)
+		return (0);
+
+	factor = 2;
+	return (checkprime(factor, n));
 
 }
