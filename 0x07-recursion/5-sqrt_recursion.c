782c80479029105cdc39762c8c9e91ccee7efcf4
Mikaelia <mikaelagurney@gmail.com>
2018-02-08 00:01:45 +0000

    Add task 5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,12 +1,33 @@
+/**
+  * increment - finds square root multiples of num
+  * @i: multiples to test
+  * @n: num to find square root of
+  *
+  * Return: square root of num
+  */
+int increment(int i, int n)
+{
+	if ((i * i) == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+		return (-1);
+	return (increment(++i, n));
+}
+
 #include "holberton.h"
 /**
+  * _sqrt_recursion - finds square root of number
+  * @n: number to find square root of
   *
   *
-  *
-  *
-  * Return:
+  * Return: square root
   */
-void _puts_recursion(char *s)
+int _sqrt_recursion(int n)
 {
+	int val;
 
+	val = 0;
+	return (increment(val, n));
 }
