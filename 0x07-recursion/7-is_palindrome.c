749656d06d3d2871d95417bba3063d500ab5ffaa
Mikaelia <mikaelagurney@gmail.com>
2018-02-08 02:08:12 +0000

    Add task 7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,12 +1,63 @@
+
+/**
+  * findlen - find length of string
+  * @s: string
+  * @count: character count
+  *
+  * Return: string length
+  */
+int findlen(char *s, int count)
+{
+	if (*s)
+	{
+		return (1 + findlen(++s, count));
+	}
+	else
+		return (count);
+}
+
+/**
+  * comphalves - compares both halves of palindrome
+  * @s: string to test
+  * @strlen: length of string
+  *
+  * Return: 1 for palindrome, 0 otherwise
+  */
+int comphalves(char *s, int strlen)
+{
+	if (*s)
+	{
+		if (*s == *(s + strlen))
+		{
+			return (1);
+			strlen--;
+			return (comphalves(++s, strlen));
+		}
+		else
+			return (0);
+	}
+	else
+		return (0);
+}
+
 #include "holberton.h"
 /**
+  * is_palindrome - checks whether string is palindrome
+  * @s: string to compare
   *
   *
-  *
-  *
-  * Return:
+  * Return: 1 for palendrome, 0 otherwise
   */
-void _puts_recursion(char *s)
+int is_palindrome(char *s)
 {
+	int count;
+	int strlen;
 
+	count = 0;
+
+	strlen = (findlen(s, count) - 1);
+
+	return (comphalves(s, strlen));
 }
+
+
