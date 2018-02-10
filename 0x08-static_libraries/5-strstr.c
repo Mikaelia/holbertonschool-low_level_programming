caee9f1b3f2fa996b51194510894444947edb5c8
Mikaelia <mikaelagurney@gmail.com>
2018-02-10 23:04:50 +0000

    remove extra function

--- 0x08-static_libraries/5-strstr.c
+++ 0x08-static_libraries/5-strstr.c
@@ -1,20 +1,4 @@
 /**
-  * _findlen -  finds length of string
-  * @s: string to count
-  *
-  * Return: num bytes in string
-  *
-  */
-int _findlen(char *s)
-{
-	int count;
-
-	for (count = 0; s[count] != '\0'; count++)
-	;
-	return (count);
-}
-
-/**
   * *_strstr -  locates a substring
   * @haystack: string to search
   * @needle: substring to search for
@@ -24,26 +8,4 @@ int _findlen(char *s)
   */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int length, count, i, j;
-
-	length = _findlen(needle);
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		j = 0;
-		count = 0;
-
-		while (needle[j] != '\0')
-		{
-			if (haystack[i + j] == needle[j])
-				count++;
-			j++;
-		}
-		if (count == length)
-		{
-			haystack = &haystack[i];
-			return (haystack);
-		}
-	}
-	return (0);
 }
--- 0x08-static_libraries/create_static_lib.sh
+++ 0x08-static_libraries/create_static_lib.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-for i in "$@";
-do
-	find ~/Projects -name "$i" -type f -exec cp {} . \;
-	gcc -c $i;
-done
 
+gcc -c *.c
 ar rc libholberton.a *.o
 ranlib libholberton.a
 
