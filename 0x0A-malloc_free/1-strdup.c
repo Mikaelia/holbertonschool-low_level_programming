522e470fe2c6af8dce7a03c7d81cd69bb42a343e
Mikaelia <mikaelagurney@gmail.com>
2018-02-14 00:52:56 +0000

    change null format

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -26,11 +26,10 @@ char *_strdup(char *str)
 	s = malloc(i * sizeof(char));
 	if (s == 0)
 		return (NULL);
-	while (j < i)
+	while (j <= i)
 	{
 		s[j] = str[j];
 		j++;
 	}
-	s[i] = '\0';
 	return (s);
 }
