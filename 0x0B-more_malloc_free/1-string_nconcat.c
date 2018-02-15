ebc2dbcf85c320c2dedbc2216931e68d98c6449b
Mikaelia <mikaelagurney@gmail.com>
2018-02-15 22:35:29 +0000

    edit null syntax

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -51,7 +51,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	total = len1 + n;
 
 	s = malloc(sizeof(char) * (total + n + 1));
-	if (!s)
+	if (s == '\0')
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
