328168d25b1bb80bff9365ebbae9d6ca55ca34e6
Mikaelia <mikaelagurney@gmail.com>
2018-02-15 20:04:36 +0000

    Fix comment spacing

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
 #include <stdlib.h>
-
- /**
-   * *malloc_checked - a function that allocates memory using malloc
-   * *b: size of space to allocate
-   *
-   * Return: pointer to the allocated memory
-   *
-   */
+/**
+  * *malloc_checked - a function that allocates memory using malloc
+  * @b: size of space to allocate
+  *
+  * Return: pointer to the allocated memory
+  */
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *s;
 
 	s = malloc(b);
 	if (s == NULL)
-		exit (98);
+		exit(98);
 	else
 		return (s);
 
