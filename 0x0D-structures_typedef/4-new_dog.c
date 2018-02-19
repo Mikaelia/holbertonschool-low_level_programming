77aa0a0a9142dc6972df621765c7422b8a398bde
Mikaelia <mikaelagurney@gmail.com>
2018-02-19 23:19:31 +0000

    Fix spacing

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,7 +12,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
-	my_dog = (dog_t*)malloc(sizeof(dog_t));
+
+	my_dog = (dog_t *)malloc(sizeof(dog_t));
+		if (my_dog == NULL)
+			return (NULL);
 	my_dog->name = name;
 	my_dog->age = age;
 	my_dog->owner = owner;
