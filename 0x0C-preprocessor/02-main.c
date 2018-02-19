44f985d41b7882fe00be4677814d20b80f8bc193
Mikaelia <mikaelagurney@gmail.com>
2018-02-19 20:00:51 +0000

    Add all tasks

---
+++ 0x0C-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - prints name of file the file was compiled from
+  *
+  *
+  *
+  * Return: 0
+  *
+  */
+int main(void){
+	printf(__FILE__"\n");
+	return (0);
+}
