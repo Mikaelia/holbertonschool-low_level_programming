0949ddad5b769695997eefe7a984c1c4f4cb80a0
Mikaelia <mikaelagurney@gmail.com>
2018-02-19 20:54:27 +0000

    Add task 1

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - dog structure
+  * @name: first member
+  * @age: second member
+  * @owner: third member
+  *
+  * Description: A structure that defines a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
