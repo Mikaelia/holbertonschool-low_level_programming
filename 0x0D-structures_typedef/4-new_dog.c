bad576b8c2c593056461c7b7bfe0e6cc1671fcdb
Mikaelia <mikaelagurney@gmail.com>
2018-02-20 01:53:21 +0000

    Refactoring 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,3 +1,55 @@
+
+/**
+  * _strlen - counts string length
+  * @s: reference string
+  *
+  * Return: NULL if function fails
+  */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * copy - copy string to allocated space
+  * @s: reference string
+  * Return: NULL if function fails
+  */
+char *copy(char *s)
+{
+	int i;
+	int length;
+	char *dest;
+
+	length = 0;
+	length = _strlen(s);
+
+	i = 0;
+	dest = malloc(sizeof(char) * length + 1);
+	if (dest == NULL)
+	{
+		free(dest);
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,6 +64,11 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	char *namecopy;
+	char *ownercopy;
+
+	namecopy = copy(name);
+	ownercopy = copy(owner);
 
 	my_dog = (dog_t *)malloc(sizeof(dog_t));
 		if (my_dog == NULL)
@@ -19,9 +76,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(my_dog);
 			return (NULL);
 		}
-	my_dog->name = name;
+	my_dog->name = namecopy;
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = ownercopy;
 
 	return (my_dog);
 }
