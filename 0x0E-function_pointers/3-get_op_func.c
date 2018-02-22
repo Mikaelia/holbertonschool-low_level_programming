f4e6232a63144a09eca2176ed8d90f64d8d57b90
Mikaelia <mikaelagurney@gmail.com>
2018-02-22 01:32:55 +0000

    Add task 3

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  * *get_op_func - selects correct function to perform
+  * @s: operator passed as argument to program
+  *
+  * Return: pointer to parameter operator function
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *(ops[i].op))
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  * main - performs and prints calculations
+  * @argc: argument count
+  * @argv: array of arguments
+  *
+  * Return: 0, exit 98 if incorrect number of arguments
+  */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+
+	result = (get_op_func(argv[2]))(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
