27091cb90c5f67cf08b6ab7866f04f275a41d3a5
Mikaelia <mikaelagurney@gmail.com>
2018-02-25 22:00:01 +0000

    move header

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,11 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#include <stdarg.h>
 /**
   * op - struct for functions
   * @c: operator to compare
