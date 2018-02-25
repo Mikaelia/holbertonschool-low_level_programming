5d8c65e28548b4c7a85335adc23ffaa4b0c93103
Mikaelia <mikaelagurney@gmail.com>
2018-02-25 21:58:21 +0000

    Add header for struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -6,11 +6,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #include <stdarg.h>
+/**
+  * op - struct for functions
+  * @c: operator to compare
+  * @p: function to call
+  */
 typedef struct op
 {
 	char *c;
 	void (*p)(va_list a);
 } prints;
+
 void printchar(va_list a);
 void printint(va_list a);
 void printfloat(va_list a);
