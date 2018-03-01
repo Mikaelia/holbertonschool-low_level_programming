2bd4c083d222c26ce200e45a031eac820adde6a4
Mikaelia <mikaelagurney@gmail.com>
2018-03-01 21:22:03 +0000

    Add task 2

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+  * *add_node - add nodes to the beginning of list
+  * @head: head of list
+  * @str: str to copy
+  *
+  * Return: number of nodes
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+		if (new == NULL)
+			return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+
+	return (new);
+}
