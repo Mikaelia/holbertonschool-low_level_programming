5768918d87ad3e7553ea109ef0bef5a4a9d0b75b
Mikaelia <mikaelagurney@gmail.com>
2018-03-01 20:46:45 +0000

    First Push

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+  * print_list - prints out all elements of list
+  * @h: pointer to node element
+  *
+  * Return: number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] ", 0);
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("[%d] ", h->len);
+			printf("%s\n", h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
