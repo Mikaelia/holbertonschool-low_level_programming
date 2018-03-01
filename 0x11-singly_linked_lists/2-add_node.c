73f519769889c63162ac941903868d4062a76880
Mikaelia <mikaelagurney@gmail.com>
2018-03-01 21:38:59 +0000

    Remove strlen function

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -12,12 +12,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int i;
 
 	new = malloc(sizeof(list_t));
 		if (new == NULL)
 			return (NULL);
+	i = 0;
+	while (str[i]!= '\0')
+	{
+		i++;
+	}
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = i;
 	new->next = *head;
 	*head = new;
 
