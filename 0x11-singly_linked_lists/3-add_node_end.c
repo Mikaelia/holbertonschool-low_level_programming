73766a9ac1d075b5dfea90e6e72d032b92418bc6
Mikaelia <mikaelagurney@gmail.com>
2018-03-01 22:20:45 +0000

    Include header null condition

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -15,6 +15,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *last;
 	int i;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
