8d2a760aec4abc87c8107ede954938a3a38c176f
Mikaelia <mikaelagurney@gmail.com>
2018-03-07 00:50:46 +0000

    remove extra else

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -21,11 +21,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (count == index)
 			return (head);
-		else
-		{
-			count++;
-			head = head->next;
-		}
+		count++;
+		head = head->next;
 	}
 
 	return (NULL);
