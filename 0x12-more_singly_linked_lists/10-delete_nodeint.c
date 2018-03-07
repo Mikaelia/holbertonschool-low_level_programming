10e0b9713cd47a9975db263c3665d0a179021a92
Mikaelia <mikaelagurney@gmail.com>
2018-03-07 00:57:55 +0000

    add extra space

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 	temp = *head;
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		if (index == 0)
 		{
