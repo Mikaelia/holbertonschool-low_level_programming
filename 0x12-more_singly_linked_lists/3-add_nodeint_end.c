27c1f2ce3bf47d4ac90b2842502c0a25e62bcd1d
Mikaelia <mikaelagurney@gmail.com>
2018-03-07 00:36:38 +0000

    Add paranthesis on return

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,11 +15,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *find;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	if (*head == NULL)
 		*head = new;
