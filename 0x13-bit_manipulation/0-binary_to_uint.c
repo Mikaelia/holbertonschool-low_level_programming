b20f05f0601f7693bda9fb6864998cf636ae0139
Mikaelia <mikaelagurney@gmail.com>
2018-03-08 21:25:10 +0000

    Remove extra line

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,11 +9,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i, j;
-	unsigned int sum, pow;
+	unsigned int sum;
+	unsigned int pow;
 	int len;
 
-	len = 0, sum = 0, i = 0, j = 0;
+	len = 0;
+	sum = 0;
 	pow = 1;
+	i = 0;
+	j = 0;
 
 	if (b == NULL)
 		return (0);
