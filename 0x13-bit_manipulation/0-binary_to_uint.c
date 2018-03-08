d51ac876e0b21f7c3bc071e611266b623c0f741f
Mikaelia <mikaelagurney@gmail.com>
2018-03-08 20:54:10 +0000

    Add task 0

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary stream
+ *
+ * Return: converted number, or zero if null or fail stream
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j;
+	unsigned int sum;
+	int pow;
+	int len;
+
+	len = 0;
+	sum = 0;
+	pow = 1;
+	i = 0;
+	j = 0;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] == '1' || b[i] == '0')
+		{
+			i++;
+			len++;
+		}
+		else
+			return (0);
+	}
+	i--;
+	while (len > 1)
+	{
+		pow *= 2;
+		len--;
+	}
+	while (i > 0)
+	{
+		sum += ((b[j] - '0') * pow);
+		pow /= 2;
+		i--;
+		j++;
+	}
+	sum += (b[j] - '0');
+	return (sum);
+}
