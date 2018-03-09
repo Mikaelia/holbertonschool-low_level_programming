1dacf9e8a2a2173cfe0fc4d6c6664037fea58e80
Mikaelia <mikaelagurney@gmail.com>
2018-03-09 01:02:01 +0000

    Add task 5

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * flip_bits - returns number of bits needed to flip to get to other number
+  * @n: first number
+  * @m: second number
+  *
+  * Return: number of bits needed to flip
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int total;
+	unsigned int sum;
+	int i;
+
+	sum = 0;
+
+	total = n ^ m;
+	for (i = 64; i > 0; i--)
+	{
+		sum += (total & 1);
+		total = total >> 1;
+	}
+
+	return (sum);
+}
