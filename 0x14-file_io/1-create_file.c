b12126b9ee811b62254faf67c68295bacc481412
Mikaelia <mikaelagurney@gmail.com>
2018-03-13 21:12:40 +0000

    Add excl permission

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		i++;
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, 0600);
 	if (fd == -1)
 		return (-1);
 	sz = write(fd, text_content, i);
