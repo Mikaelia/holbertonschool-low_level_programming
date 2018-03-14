d0b8541a80ea60a5440d8b73575a537605926201
Mikaelia <mikaelagurney@gmail.com>
2018-03-14 01:01:19 +0000

    change sz to numred

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -59,7 +59,7 @@ int copy_textfile(const char *filefrom, char *fileto)
 			exit(99);
 		}
 	}
-	if (sz == -1)
+	if (numred == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filefrom);
 		exit(98);
