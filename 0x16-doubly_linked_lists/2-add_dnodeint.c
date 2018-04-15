e31e6f32c022ad838315e01bd44e4dee28fa505f
Mikaelia <mikaelagurney@gmail.com>
2018-04-15 15:33:19 +0000

    make task 2 more concise

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -15,19 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (*head == NULL)
-	{
-		new->next = *head;
-		new->prev = NULL;
-		*head = new;
-		return (*head);
-	}
-	else
-	{
-		new->next = *head;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head != NULL)
 		(*head)->prev = new;
-		new->prev = NULL;
-		*head = new;
-		return (*head);
-	}
+	*head = new;
+	return (*head);
 }
