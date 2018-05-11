a1d52f076a47d8c28e5682adb072404b5d1eb916
Mikaelia <mikaelagurney@gmail.com>
2018-05-11 16:02:21 +0000

    remove key check

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,8 +12,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *head;
 	hash_node_t *new;
 
-	if (key == NULL)
-		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
 	head = ht->array[idx];
 
