61dfc4d04738857e8e17ee9ca1811066e666fad1
Mikaelia <mikaelagurney@gmail.com>
2018-05-11 17:42:13 +0000

    new push

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
   */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t  *new;
+	hash_table_t  *new = NULL;
 	unsigned long int i = 0;
 
 	new = malloc(sizeof(hash_table_t));
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	new->size = size;
 
-	new->array  = malloc(sizeof(hash_node_t *) * size);
+	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (new->array == NULL)
 	{
 		free(new);
