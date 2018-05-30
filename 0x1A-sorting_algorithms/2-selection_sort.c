6b287ba0a678810271a0dee028689b06237048b0
Mikaelia <mikaelagurney@gmail.com>
2018-05-30 18:19:04 +0000

    betty fixes

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -6,25 +6,27 @@
   */
 void selection_sort(int *array, size_t size)
 {
-    int i, j;
-    int new[size];
-    int tmp;
+	int i, j;
+	int new[size];
+	int tmp;
 	int idx_min;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        idx_min= i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[idx_min])
-			{
-            	idx_min = j;
-			}
-        }
-        tmp = array[idx_min];
-        array[idx_min] = array[i];
-        array[i] = tmp;
-		print_array(array, size);
-    }
-}
+	if (array == NULL || size < 2)
+		return;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		idx_min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[idx_min])
+			{
+				idx_min = j;
+			}
+		}
+		tmp = array[idx_min];
+		array[idx_min] = array[i];
+		array[i] = tmp;
+		print_array(array, size);
+	}
+}
