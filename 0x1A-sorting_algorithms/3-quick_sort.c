4291235758677623c4539d41d7d15f7f3eb59f52
Mikaelia <mikaelagurney@gmail.com>
2018-05-30 20:22:54 +0000

    change size constraint

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -67,7 +67,7 @@ void quicksort(int *array, int lo, int hi, size_t size)
   */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 	quicksort(array, 0, size - 1, size);
 }
