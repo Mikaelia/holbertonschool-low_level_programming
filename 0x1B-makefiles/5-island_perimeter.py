403686862272aa54bd6172682cf8f77cb3b5df93
Mikaelia <mikaelagurney@gmail.com>
2018-06-16 02:06:57 +0000

    Update to address external moat

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -9,16 +9,19 @@ def island_perimeter(grid):
     total = 0
     for x in grid:
         width = len(x)
-    if width > 2 and height > 2:
-        for x in range(width - 2):
+    if width > 0 and height > 0:
+        for x in range(width - 1):
             prev.append(0)
-        for h in range(1, height - 1):
-            for w in range(1, width - 1):
+        for h in range(height):
+            for w in range(width):
                 if grid[h][w]:
                     total += 4
-                    if prev[w - 1]:
-                        total -= 2
-                    if grid[h][w - 1]:
-                        total -= 2
-                prev[w - 1] = grid[h][w]
+                    if height * width == 1:
+                        return
+                    if w > 0:
+                        if prev[w - 1]:
+                            total -= 2
+                        if grid[h][w - 1]:
+                            total -= 2
+                        prev[w - 1] = grid[h][w]
     return(total)
