55e36a630e78fc32419462e6b357df8478f3eecd
Mikaelia <mikaelagurney@gmail.com>
2018-06-16 00:07:30 +0000

    Add task 5

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Island perimeter algorithm'''
+def island_perimeter(grid):
+    height = len(grid)
+    prev = []
+    total = 0
+    for x in grid:
+        width = len(x)
+    if width > 2 and height > 2:
+        for x in range(width - 2):
+            prev.append(0)
+        for h in range(1, height - 1):
+            for w in range(1, width - 1):
+                if grid[h][w]:
+                    total += 4
+                    if prev[w-1]:
+                        total -= 2
+                    if grid[h][w-1]:
+                        total -= 2
+                prev[w-1] = grid[h][w]
+    return(total)
