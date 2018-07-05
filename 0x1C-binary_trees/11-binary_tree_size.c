9a6fd43fad1a512765b911d6453d67d6b4875f13
Mikaelia <mikaelagurney@gmail.com>
2018-07-05 21:42:26 +0000

    Rewrite task 11

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -1,32 +1,17 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_is_leaf - checks if a node is a leaf
-  * @node: pointer to the node to check
-  * Return: 1 if node is a leaf, otherwise 0
-  */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (!node || node->left || node->right)
-		return (0);
-	else
-		return (1);
-}
-/**
-  * binary_tree_size - measures the size of a binary tree
-  * @tree: pointer to node to measure size of the tree
-  * Return: 0 if tree is NULL
-  */
-size_t binary_tree_size(const binary_tree_t *tree)
+ * binary_tree_size - measures the size of tree;
+ * @node: the node to check size
+ * Return: returns the size of the node
+ */
+size_t binary_tree_size(const binary_tree_t *node)
 {
 	int size;
 
-
-	if (!tree)
+	if (!node)
 		return (0);
-	if (binary_tree_is_leaf(tree))
-		return (1);
-	size = 0;
-	size += binary_tree_size(tree->left);
-	size += (1 + binary_tree_size(tree->right));
+	size = 1;
+	size += binary_tree_size(node->left);
+	size += binary_tree_size(node->right);
 	return (size);
 }
