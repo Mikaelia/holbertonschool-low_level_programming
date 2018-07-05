4a4e87522c137d39b834f179bc8d84be79b53d99
Mikaelia <mikaelagurney@gmail.com>
2018-07-05 21:25:18 +0000

    Remove extra lines

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -8,16 +8,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || node->parent == NULL)
 		return (NULL);
-	if (node->parent->left && node->parent->right)
-	{
-		if (node->parent->left == node)
-			return (node->parent->right);
-		else if (node->parent->right == node)
-			return (node->parent->left);
-		else
-			return (node);
-	}
-	else
-		return (NULL);
-
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
