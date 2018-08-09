983e219e9343ff683845f583a4eebf19845e9a49
Mikaelia <mikaelagurney@gmail.com>
2018-08-09 17:29:32 +0000

    Add task 2

--- 0x1D-search_algorithms/1-main.c
+++ 0x1D-search_algorithms/1-main.c
@@ -15,7 +15,7 @@ int main(void)
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
-    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
-    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
+    printf("Found %d at index: %d\n\n", 5, binary_search(array, 6, 5));
+    printf("Found %d at index: %d\n", 9, binary_search(array, size, 9));
     return (EXIT_SUCCESS);
 }
